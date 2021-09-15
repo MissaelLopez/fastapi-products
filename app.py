7dd4e7c6896e74b503ed122a5f18b04275b3ee50
MissaelLopez <missaelnj8@gmail.com>
2021-09-15 18:05:19 -0500

    Fix api routes

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 from typing import Optional, Text
 from datetime import datetime
 from uuid import uuid4 as uuid
+from starlette.requests import Request
+from starlette.responses import Response
 import uvicorn
 
 app = FastAPI()
@@ -10,34 +12,40 @@ app = FastAPI()
 products = []
 
 # Products Model
+
+
 class Product(BaseModel):
     id: Optional[str]
     name: str
-    prize: float
+    price: float
     stock: int
 
-@app.get("/")
-def read_root():
-    return {"welcome": "Welcome to my API"}
 
-@app.get("/products")
+@app.get("/api")
+def api_root():
+    return {"welcome": "Welcome to my FastAPI"}
+
+@app.get("/api/products")
 def get_products():
     return products
 
-@app.post("/products")
+
+@app.post("/api/products")
 def save_product(product: Product):
     product.id = str(uuid())
     products.append(product.dict())
     return products[-1]
 
-@app.get("/products/{product_id}")
+
+@app.get("/api/products/{product_id}")
 def get_product(product_id: str):
     for product in products:
         if product["id"] == product_id:
             return product
     raise HTTPException(status_code=404, detail="Product not found")
 
-@app.put("/products/{product_id}")
+
+@app.put("/api/products/{product_id}")
 def update_product(product_id: str, update_product: Product):
     for index, product in enumerate(products):
         if product["id"] == product_id:
@@ -47,7 +55,8 @@ def update_product(product_id: str, update_product: Product):
             return {"message": "Product has been updated successfully"}
     raise HTTPException(status_code=404, detail="Product not found")
 
-@app.delete("/products/{product_id}")
+
+@app.delete("/api/products/{product_id}")
 def delete_product(product_id: str):
     for index, product in enumerate(products):
         if product["id"] == product_id:
